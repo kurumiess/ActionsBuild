8dbba9b0556eae80e3ab46944fb1d6b258237519
KURUMI <60105584+kurumiess@users.noreply.github.com>
2022-07-04 21:47:20 +0800

    Update make_after.sh

--- REDMI-ac2100/make_after.sh
+++ REDMI-ac2100/make_after.sh
@@ -1,5 +1,1 @@
 #!/bin/bash
-
-# 调整文件助手菜单
-sed -i 's/nas/system/g' package/feeds/luci/applications/luci-app-fileassistant/luasrc/controller/*.lua
-sed -i 's/nas/system/g' package/feeds/luci/applications/luci-app-fileassistant/luasrc/view/*.htm
